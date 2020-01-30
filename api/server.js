4520f80969e4855827bafb9534c0f157f1e38c5b
perezsergioace <perez.sergio.ace@gmail.com>
2020-01-30 18:26:29 -0500

    setup files for tests. ran first test to check if server runs the tests

--- api/server.js
+++ api/server.js
@@ -1,7 +1,23 @@
 const express = require('express');
 
+const Users = require('../users/usersModel');
+
 const server = express();
 
 server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: "up" })
+})
+
+server.get('/users', (req, res) => {
+    Users.getAll()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(error => {
+            res.status(500).json({error: "error retrieving usres"}, error)
+        })
+})
 
 module.exports = server;

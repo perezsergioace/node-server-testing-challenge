d0ed1369768f17d2f0e705d5e7b3775571fb9ca9
perezsergioace <perez.sergio.ace@gmail.com>
2020-01-30 17:55:45 -0500

    added appropriate code o the users migration file

--- data/migrations/20200130175223_users.js
+++ data/migrations/20200130175223_users.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("users", tbl => {
+      tbl.increments();
+
+      tbl.string("name", 128).notNullable();
+  })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists("users");
 };

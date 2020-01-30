3003425752edebedc10b14b6a69f516bf1fb0eb9
perezsergioace <perez.sergio.ace@gmail.com>
2020-01-30 18:00:07 -0500

    created the test.db3 and the users.db3 files using the knex migrate:latest and knex migrate:latest --env testing commands

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/users.db3'
     },
     pool: {
       afterCreate: (conn, done) => {
